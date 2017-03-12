67ce22644428cbf828e9d06469d06215a76d5fd1
caoli5288 <caoli5288@gmail.com>
2017-03-13 03:36:09 +0800

    Fixed #1

--- src/com/i5mc/idea/decompiler/Impl.java
+++ src/com/i5mc/idea/decompiler/Impl.java
@@ -1,16 +1,21 @@
 package com.i5mc.idea.decompiler;
 
+import com.google.common.collect.ImmutableMap;
 import org.benf.cfr.reader.PluginRunner;
+
+import java.util.Map;
 
 /**
  * Created on 17-3-8.
  */
 public interface Impl {
 
+    Map<String, String> OPTION = ImmutableMap.of("hideutf", "false");
+
     String decompile(String path);
 
     static String fuck(String path) {
-        PluginRunner r = new PluginRunner();
+        PluginRunner r = new PluginRunner(OPTION);
         r.addJarPath(path);
         return r.getDecompilationFor(path);
     }
@@ -18,7 +23,7 @@ public interface Impl {
     static String clazz(String path) {
         int i = path.lastIndexOf("!/");// filesystem path can contain ! but java can not
         if (i == -1) throw new IllegalArgumentException();
-        PluginRunner r = new PluginRunner();
+        PluginRunner r = new PluginRunner(OPTION);
         String head = path.substring(0, i);
         r.addJarPath(head);
         String tail = path.substring(i + 2, path.length() - 6);
