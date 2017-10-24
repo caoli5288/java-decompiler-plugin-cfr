525d08abe7703a553d14c5aa90c2552276b6757c
Zhang <caoli5288@gmail.com>
2017-10-24 15:25:09 +0800

    Fix #2

--- src/com/i5mc/idea/decompiler/Impl.java
+++ src/com/i5mc/idea/decompiler/Impl.java
@@ -11,6 +11,7 @@ import java.util.Map;
 public interface Impl {
 
     Map<String, String> OPT = new HashMap<String, String>() {{
+        put("comments", "false");
         put("hideutf", "false");
     }};
 
